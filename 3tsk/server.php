e26fdd907b2d84c35ff2dab1db893fc7670af884
formleszs <144155832+formleszs@users.noreply.github.com>
2024-04-03 21:54:41 +0300

    Update server.php

--- 3tsk/server.php
+++ 3tsk/server.php
@@ -88,7 +88,7 @@
       <div class="error">
         <?php if ($error !== ""): ?>
           <?php echo $error ?>
-          <a class="btn" href="javascript:history.back()"><input type="button" value="Вернутся"></input></a>
+          <a class="btn" href="javascript:history.back()"><input type="button" value="Вернуться"></input></a>
         <?php endif;?>
       </div>
 
@@ -132,7 +132,7 @@
               </div>
           <?php endwhile; ?>
         </div>
-        <a class="btn" href="javascript:history.back()"><input type="button" value="Вернутся"></input></a>
+        <a class="btn" href="javascript:history.back()"><input type="button" value="Вернуться"></input></a>
       <?php endif; ?>
 </body>
 </html>
